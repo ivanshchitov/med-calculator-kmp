63c54ad5c81a78c52600d3842711397b4ef56b9f
Ivan Shchitov <ivan.shchitov@fruct.org>
2026-06-21 19:16:36 +0300

    Implement the Koin dependency injection

---
+++ shared/src/iosMain/kotlin/org/dishch/medcalculator/di/Koin.ios.kt
@@ -0,0 +1,8 @@
+package org.dishch.medcalculator.di
+
+import org.dishch.medcalculator.data.local.getDatabaseBuilder
+import org.koin.dsl.module
+
+actual fun platformModule() = module {
+    single { getDatabaseBuilder() }
+}
