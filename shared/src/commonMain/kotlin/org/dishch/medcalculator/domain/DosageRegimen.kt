b9dda6f6bd7c37309206400d3419f2ce0f5ce34d
Ivan Shchitov <ivan.shchitov@fruct.org>
2026-06-23 22:38:56 +0300

    Implement retrieving dosage regimen by medication id from database

---
+++ shared/src/commonMain/kotlin/org/dishch/medcalculator/domain/DosageRegimen.kt
@@ -0,0 +1,10 @@
+package org.dishch.medcalculator.domain
+
+data class DosageRegimen(
+    val id: Long,
+    val fromAge: Int,
+    val toAge: Int,
+    val minDosePerKg: Double,
+    val maxDosePerKg: Double,
+    val medicationId: Long
+)
