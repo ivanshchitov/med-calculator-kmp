1e24d35805524450c3d09bcd6b10b00eec1642b0
Ivan Shchitov <ivan.shchitov@fruct.org>
2026-06-23 23:58:27 +0300

    Move string literals from MedcicationInfoBottomSheet into resources

--- shared/src/commonMain/kotlin/org/dishch/medcalculator/domain/DosageRegimen.kt
+++ shared/src/commonMain/kotlin/org/dishch/medcalculator/domain/DosageRegimen.kt
@@ -8,3 +8,20 @@ data class DosageRegimen(
     val maxDosePerKg: Double,
     val medicationId: Long
 )
+
+val DosageRegimen.formattedMinDose: String
+    get() = if (minDosePerKg % 1.0 == 0.0) {
+        minDosePerKg.toInt().toString()
+    } else {
+        minDosePerKg.toString()
+    }
+
+val DosageRegimen.formattedMaxDose: String
+    get() = if (maxDosePerKg % 1.0 == 0.0) {
+        maxDosePerKg.toInt().toString()
+    } else {
+        maxDosePerKg.toString()
+    }
+
+val DosageRegimen.formattedDoseRange: String
+    get() = "$formattedMinDose-$formattedMaxDose"
