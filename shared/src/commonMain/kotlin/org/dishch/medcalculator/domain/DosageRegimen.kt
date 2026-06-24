508da278f77aa136bb64c91be039f1658006722e
Ivan Shchitov <ivan.shchitov@fruct.org>
2026-06-24 12:35:21 +0300

    Implement DosageRegimen.formattedAgeRange extension property

--- shared/src/commonMain/kotlin/org/dishch/medcalculator/domain/DosageRegimen.kt
+++ shared/src/commonMain/kotlin/org/dishch/medcalculator/domain/DosageRegimen.kt
@@ -1,6 +1,10 @@
 package org.dishch.medcalculator.domain
 
+import androidx.compose.runtime.Composable
+import medcalculator.shared.generated.resources.Res
+import medcalculator.shared.generated.resources.age_range_format
 import org.dishch.medcalculator.formattedDouble
+import org.jetbrains.compose.resources.stringResource
 
 data class DosageRegimen(
     val id: Long,
@@ -19,3 +23,19 @@ val DosageRegimen.formattedMaxDose: String
 
 val DosageRegimen.formattedDoseRange: String
     get() = "$formattedMinDose-$formattedMaxDose"
+
+val DosageRegimen.formattedAgeRange: String
+    @Composable
+    get() = when {
+        toAge >= ADULT_AGE_MONTHS -> fromAge.toAge().formattedAgeLimit
+        else -> {
+            var startAgeString = fromAge.toAge().quantity.toString()
+            if (fromAge.toAge().isYears != toAge.toAge().isYears) {
+                startAgeString = fromAge.toAge().toPluralsString()
+            }
+            stringResource(Res.string.age_range_format,
+                startAgeString,
+                toAge.toAge().toPluralsString()
+            )
+        }
+    }
