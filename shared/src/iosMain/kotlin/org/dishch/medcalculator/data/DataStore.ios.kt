97ecda7a8a3c314701c33beaf6fc553e1536f9b4
Ivan Shchitov <ivan.shchitov@fruct.org>
2026-07-10 23:07:08 +0300

    Fix creating database for iOS

--- shared/src/iosMain/kotlin/org/dishch/medcalculator/data/DataStore.ios.kt
+++ shared/src/iosMain/kotlin/org/dishch/medcalculator/data/DataStore.ios.kt
@@ -1,13 +1,18 @@
 package org.dishch.medcalculator.data
 
-import platform.Foundation.*
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSUserDomainMask
 
+@OptIn(ExperimentalForeignApi::class)
 actual fun dataStorePath(): String {
     val documentDirectory = NSFileManager.defaultManager.URLForDirectory(
-        NSDocumentDirectory,
-        NSUserDomainMask,
-        true,
-        null
+        directory = NSDocumentDirectory,
+        inDomain = NSUserDomainMask,
+        appropriateForURL = null,
+        create = false,
+        error = null
     )
     return requireNotNull(documentDirectory?.path) + "/" + dataStoreFileName
 }
