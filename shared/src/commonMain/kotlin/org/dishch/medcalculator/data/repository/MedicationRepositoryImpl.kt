ecff24205a1bc8faa62fd48afa1c23b438384be9
Ivan Shchitov <ivan.shchitov@fruct.org>
2026-07-12 14:43:43 +0300

    Convert MedicationEntity to Medication inside MedicationRepository

--- shared/src/commonMain/kotlin/org/dishch/medcalculator/data/repository/MedicationRepositoryImpl.kt
+++ shared/src/commonMain/kotlin/org/dishch/medcalculator/data/repository/MedicationRepositoryImpl.kt
@@ -4,7 +4,6 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import org.dishch.medcalculator.data.local.DosageRegimenEntity
 import org.dishch.medcalculator.data.local.MedicationDao
-import org.dishch.medcalculator.data.local.MedicationEntity
 import org.dishch.medcalculator.data.local.toDomain
 import org.dishch.medcalculator.domain.model.Medication
 import org.dishch.medcalculator.domain.repository.LocaleRepository
@@ -15,16 +14,16 @@ class MedicationRepositoryImpl(
     private val localeRepository: LocaleRepository
 ) : MedicationRepository {
 
-    override fun getAllMedications(): Flow<List<MedicationEntity>> =
-        dao.getAllMedications()
+    override fun getAllMedications(): Flow<List<Medication>> =
+        dao.getAllMedications().map { entities ->
+            entities.map { it.toDomain(languageCode = localeRepository.getCurrentLanguageCode()) }
+        }
 
     override fun getRegimensByMedicationId(medicationId: String): Flow<List<DosageRegimenEntity>> =
         dao.getRegimensByMedicationId(medicationId)
 
     override fun getMedicationById(medicationId: String): Flow<Medication?> =
         dao.getMedicationById(medicationId).map {
-            it?.toDomain(
-                languageCode = localeRepository.getCurrentLanguageCode()
-            )
+            it?.toDomain(languageCode = localeRepository.getCurrentLanguageCode())
         }
 }
