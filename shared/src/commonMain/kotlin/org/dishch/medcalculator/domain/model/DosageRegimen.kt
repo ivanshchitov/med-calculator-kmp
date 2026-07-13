0e86f3b3c7ca2829fd94c3bf88369dddd0e2ff1e
Ivan Shchitov <ivan.shchitov@fruct.org>
2026-07-13 12:33:17 +0300

    Init the Domain Route enum with string resources for representation

--- shared/src/commonMain/kotlin/org/dishch/medcalculator/domain/model/DosageRegimen.kt
+++ shared/src/commonMain/kotlin/org/dishch/medcalculator/domain/model/DosageRegimen.kt
@@ -1,6 +1,16 @@
 package org.dishch.medcalculator.domain.model
 
-enum class Route { IV, IM, SC }
+import medcalculator.shared.generated.resources.Res
+import medcalculator.shared.generated.resources.route_im
+import medcalculator.shared.generated.resources.route_iv
+import medcalculator.shared.generated.resources.route_sc
+import org.jetbrains.compose.resources.StringResource
+
+enum class Route(val stringRes: StringResource) {
+    IV(Res.string.route_iv),
+    IM(Res.string.route_im),
+    SC(Res.string.route_sc);
+}
 enum class DosageUnit { MG, MG_PER_KG }
 
 
