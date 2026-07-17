e4125010344f4db23308d53ea03c9b0a932df364
Ivan Shchitov <ivan.shchitov@fruct.org>
2026-07-17 15:14:56 +0300

    Implement the custom text field ot set up its content paddings

--- shared/src/commonMain/kotlin/org/dishch/medcalculator/ui/components/cards/AppCard.kt
+++ shared/src/commonMain/kotlin/org/dishch/medcalculator/ui/components/cards/AppCard.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -88,6 +89,7 @@ fun AppCard(
                         style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
                         color = AppColors.TextPrimary
                     )
+                    Spacer(modifier = Modifier.height(AppDimens.SpacingExtraSmall))
                     content()
                 }
 
