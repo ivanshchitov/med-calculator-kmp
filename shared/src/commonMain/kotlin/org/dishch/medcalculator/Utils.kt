ebe1c6dce5755b2e102b8b326555546a851e34dd
Ivan Shchitov <ivan.shchitov@fruct.org>
2026-07-20 22:08:18 +0300

    Update the medications data

--- shared/src/commonMain/kotlin/org/dishch/medcalculator/Utils.kt
+++ shared/src/commonMain/kotlin/org/dishch/medcalculator/Utils.kt
@@ -3,7 +3,7 @@ package org.dishch.medcalculator
 import kotlin.math.roundToInt
 
 fun Double.formatAsDecimal(): String {
-    val rounded = (this * 100).roundToInt() / 100.0
+    val rounded = (this * 1000).roundToInt() / 1000.0
     return if (rounded % 1.0 == 0.0) {
         rounded.toLong().toString()
     } else {
